7a0903d28ab0c6964c34ccdfc5903a9a7d0e702f
Cesar Castillo <castillocesar2412@gmail.com>
2023-03-15 00:10:59 -0500

    added code to navbar.js and added photos in the images folder in assets folder

--- portfolio/src/Components/navbar.js
+++ portfolio/src/Components/navbar.js
@@ -16,14 +16,17 @@ export const NavBar = () => {
             }
         };
         window.addEventListener('scroll', onScroll);
-        return () => {
-            window.removeEventListener('scroll', onScroll);
-        };
+        return () =>   window.removeEventListener('scroll', onScroll);
+
     }, []);
+
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
 
 
     return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" className={ scrolled ? 'scrolled': ""}>
       <Container>
         <Navbar.Brand href="#home">Home</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
@@ -31,9 +34,10 @@ export const NavBar = () => {
             </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            <Nav.Link href="#resume" className={activeLink === 'resume' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('resume')}>Resume</Nav.Link>
             </Nav>
             <span className="navbar-text">
                 <div className="social-icon">
