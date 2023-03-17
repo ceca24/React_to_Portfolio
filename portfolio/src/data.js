4b886241654d38c9947e367bc2bdeb1cfdd8d26c
Cesar Castillo <castillocesar2412@gmail.com>
2023-03-16 20:36:51 -0500

    added images to the images folder and added necessary scripts to the data.js file to connect with the projects.js file

---
+++ portfolio/src/data.js
@@ -0,0 +1,50 @@
+export const projects = [
+    {
+      title: "Project SafeNyt",
+      subtitle: "Interactive Front-End Application",
+      description:
+        "This first group project was a collaboration between myself and four other people. IT was created using HTML, CSS, JavaScript and APIs.",
+      image: "./images/SafeNyt.png",
+      link: "https://ceca24.github.io/Project-Safe-Nyt/",
+    },
+    {
+      title: "Weather Dashboard",
+      subtitle: "Server-Side APIs",
+      description:
+        "This project was created using HTML, CSS, JavaScript and APIs. It allows the user to search for a city and get the current weather and a five day forecast.",
+      image: "./images/weatherdashboard.png",
+      link: "https://ceca24.github.io/weather-dashboard/",
+    },
+    {
+      title: "Legend of Nelda",
+      subtitle: "Interactive Full-Stack Application",
+      description:
+        "This project was a collaboration between myself and three other people. This is a full-stack application that allows the user to create an account, login, and play a game. The game is a Zelda themed game that allows the user to move around the map and fight enemies. The user can also save their progress and come back to it later.",
+      image: "./images/LegendofNelda.png",
+      link: "https://legendofnelda.herokuapp.com/",
+    },
+    {
+      title: "Note Taker App",
+      subtitle: "Express.js",
+      description:
+        "This project was created using HTML, CSS, JavaScript, Node.js, Express.js, and APIs. It allows the user to create, save, and delete notes.",
+      image: "./images/notetakerapp.png",
+      link: "https://ancient-thicket-73424.herokuapp.com/",
+    },
+    {
+      title: "Operation: Wellness",
+      subtitle: "Interactive Full-Stack Application",
+      description:
+        "This project was a collaboration between myself and four other people. This is a full-stack application that allows the user to create an account, login, and add any medication/supplements they are taking. It helps the user keep track of their medications and supplements and also helps them keep track of when they need to take them.",
+      image: "./images/ReactstreetBoys.png",
+      link: "https://operation-wellness-rsb.herokuapp.com/",
+    },
+    {
+      title: "Knowledge Check",
+      subtitle: "Web APIs",
+      description:
+        "This project was created using HTML, CSS, JavaScript, and APIs. It allows the user to take a quiz and get a score at the end. The user can also save their score and initials to the local storage.",
+      image: "./images/knowledgecheck.png",
+      link: "https://ceca24.github.io/knowledge-check/",
+    },
+    ];
