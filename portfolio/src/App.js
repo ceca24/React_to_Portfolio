b74a11fd9c43d1e65e77e5241a300e7c499ef82d
Cesar Castillo <castillocesar2412@gmail.com>
2023-03-16 19:22:31 -0500

    added the import code for each component in app.js file

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
 
 export default function App() {
   return (
